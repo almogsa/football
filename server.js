ffa5af1a6e96d699090587543a7ee890f80ab1b5
sadea <almog@hpe.com>
2017-10-23 10:26:42 +0300

    express settings

--- server.js
+++ server.js
@@ -17,9 +17,11 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+const root = './';
 
-app.use(express.static('www'));
-app.set('port', process.env.PORT || 80);
+//app.use(express.static('www'));
+app.use(express.static(path.join(root, 'www')));
+app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
